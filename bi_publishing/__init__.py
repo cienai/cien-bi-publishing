bb0a4b2becc4310eafdc5da0f3838a9216b1bd03
Barath Bheeman <23279260+barathbheeman@users.noreply.github.com>
2024-04-22 16:09:28 +0200

    Update __init__.py

--- bi_publishing/__init__.py
+++ bi_publishing/__init__.py
@@ -446,3 +446,36 @@ def download_file_from_integration_hub(filename, local_file_name):
     url = "https://github.com/cienai/IntegrationHub/raw/main/powerbi/" + encoded_file
     r = requests.get(url, allow_redirects=True)
     open(local_file_name, 'wb').write(r.content)
+
+
+def get_capcities(client):
+    api_url = f"{POWERBI_BASE_URL}/capacities"
+    response = requests.get(api_url, headers=_get_headers(client))
+    if response.status_code == 200:
+        capacities = response.json()
+        return capacities['value']
+    else:
+        raise Exception(response.content)
+
+
+def get_capacity(client, capacity_name):
+    capacities = get_capcities(client)
+    for capacity in capacities:
+        if capacity['displayName'] == capacity_name:
+            return capacity
+    raise ValueError(f"capacity: {capacity_name} not found")
+
+
+def add_group_to_capacity(client, group_id, capacity_id):
+    """
+    Add the given group/workspace to the given capacity
+    """
+    api_url = f"{POWERBI_BASE_URL}/groups/{group_id}/AssignToCapacity"
+    print(f"--- adding group: {group_id} to dataset: {capacity_id} ---")
+    print(f"--- add url: {api_url} ---")
+    body = {'capacityId': capacity_id}
+    response = requests.post(api_url, headers=_get_headers(client), data=json.dumps(body))
+    if response.ok:
+        print("--- add successful ---")
+    else:
+        raise Exception(f"--- add failed: {response.content} ---")
