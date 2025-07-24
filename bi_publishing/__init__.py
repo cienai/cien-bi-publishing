060a57c45e66900fff66bfeb47b4a3dc392cc566
Barath Bheeman <23279260+barathbheeman@users.noreply.github.com>
2025-07-24 13:40:28 +0530

    Update __init__.py

--- bi_publishing/__init__.py
+++ bi_publishing/__init__.py
@@ -394,9 +394,10 @@ def update_dataset_params(client, db_name, dw_conn, group_id, dataset_id):
     }
 
     if db_type == "Azure Data Lake":
-        file_server_data_lake, file_folder_data_lake = dw_conn['BUCKET_URI'].replace('blob', 'dfs').replace('wasbs://', '').split('/')
-        file_server_data_lake = f'https://{file_server_data_lake}/'
-        file_folder_data_lake += '/export/'
+        file_path = dw_conn['BUCKET_URI'].replace('blob', 'dfs').replace('wasbs://', '')
+        file_server_data_lake = 'https://' + file_path.split('/')[0] + '/'
+        file_folder_data_lake = '/'.join(file_path.split('/')[1:]) + '/export/'
+
         details['updateDetails'].append({"name": 'file_server_data_lake', "newValue": file_server_data_lake})
         details['updateDetails'].append({"name": 'file_folder_data_lake', "newValue": file_folder_data_lake})
 
